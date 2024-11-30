09c6ea2f83907f53f95a46e373cf3568390c6956
Tim Kneisel <mail@tim-kneisel.de>
2024-12-01 00:46:53 +0100

    make things a little prettier

--- src/app/scoreOverview/score-overview.component.ts
+++ src/app/scoreOverview/score-overview.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ChartHeatmapComponent } from "../chart-heatmap/chart-heatmap.component";
 import { DataSelectorComponent } from "../data-selector/data-selector.component";
-import { resolveHeatmapData } from '../../dataResolvers/HeatmapResolver';
 
 @Component({
   selector: 'app-score-overview',
   standalone: true,
   imports: [ChartHeatmapComponent, DataSelectorComponent],
-  templateUrl: './score-overview.component.html'
+  templateUrl: './score-overview.component.html',
+  styleUrl: './score-overview.component.scss'
 })
 export class ScoreOverviewComponent { }
